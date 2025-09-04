f92d1bf486bdae1e7c1b0628d10e1bd616848076
Alexa DeRosa <aderosa@mapc.org>
2025-09-04 10:06:23 -0400

    updating to include fields for 2010 census tract, house district, and senate district

--- Data Cleaning and Classification/5_warren-group-mapc-submarket-join.R
+++ Data Cleaning and Classification/5_warren-group-mapc-submarket-join.R
@@ -1,12 +1,13 @@
 ### Warren Group MAPC Housing Submarket spatial join
 ### Author: Seleeke Flingai
 ### Date: 04/29/2020
-### Purpose: Identifies which MAPC Housing Submarket a given property resides.
-#Note - previously script 6
+### Purpose: Identifies which MAPC Housing Submarket a given property resides. Addition to also
+### identify 2010 census tract and legislative district for each purchase
+#Note - previously script 6; takes 13 minutes to run
 
 rm(list=ls())
 gc()
-pacman::p_load(tidyverse, data.table, bit64, sf, sp, reshape, assertr, assertthat, udpipe)
+pacman::p_load(tidyverse, data.table, bit64, sf, sp, reshape, assertr, assertthat, udpipe, mapcdatakeys, arcgis)
 options('scipen' = 10)
 
 #data paths
@@ -26,7 +27,7 @@ submarket_shp = st_read("20200109_mapc-housing-submarkets.shp") |>
 setwd(data)
 #list.files()
 #changed from fread to csv because data was being lost
-warren <- read_csv("20241220_warren_speculative-investment-buyer-sort-analysis-dataset.csv") 
+warren <- read_csv("20250903_warren_speculative-investment-buyer-sort-analysis-dataset.csv") 
 
 # warren_id <- warren |> 
 #   #create unique ID to join on - when using data inclusive of 2023 on this field should already exist
@@ -47,10 +48,10 @@ warren_pts <- warren |>
   st_transform(mass_mainland, crs = st_crs(submarket_shp))
 
 #subsetting points to those within submarket to save time
-warren_pts <- warren_pts[submarket_shp,]
+warren_pts_mapc <- warren_pts[submarket_shp,]
 
 #place warren pts in submarkets
-warren_submarket <- warren_pts |> 
+warren_submarket <- warren_pts_mapc |> 
   st_join(submarket_shp[, 'class_reor']) |> 
   dplyr::rename(mapc_submarket = class_reor) |> 
   st_drop_geometry() |> 
@@ -58,12 +59,53 @@ warren_submarket <- warren_pts |>
 
 #add submarket data back in to full data frame
 warren_submarket_final <- left_join(warren, warren_submarket, by = 'id')
+rm(warren, warren_submarket, submarket_shp, warren_pts_mapc)
+gc()
+
+#place warren pts in 2010 census tracts
+ct_2010 <- mapcdatakeys::tract_sf(2010)
+
+warren_ct2010 <- warren_pts |> 
+  st_join(ct_2010[, 'ct10_id']) |> 
+  st_drop_geometry() |> 
+  select(id, ct10_id)
+
+#add census tract data back in to full data frame
+warren_ct2010_final <- left_join(warren_submarket_final, warren_ct2010, by = 'id')
+rm(warren_submarket_final, warren_ct2010, ct_2010)
+
+#place warren pts in house districts
+house_shp <- st_read("HOUSE2021/HOUSE2021/HOUSE2021_POLY.shp")
+
+warren_house <- warren_pts |> 
+  st_join(house_shp[, 'REP_DIST']) |> 
+  dplyr::rename(house_dist = REP_DIST) |>
+  st_drop_geometry() |> 
+  select(id, house_dist)
+
+#add house data back in to full data frame
+warren_house_final <- left_join(warren_ct2010_final, warren_house, by = 'id')
+rm(warren_ct2010_final, warren_house, house_shp)
+
+#place warren pts in senate districts
+senate_shp <- st_read("SENATE2021/SENATE2021_POLY.shp")
+
+warren_senate <- warren_pts |> 
+  st_join(senate_shp[, 'SEN_DIST']) |> 
+  dplyr::rename(sen_dist = SEN_DIST) |>
+  st_drop_geometry() |> 
+  select(id, sen_dist)
+
+#add house data back in to full data frame
+warren_join_final <- left_join(warren_house_final, warren_senate, by = 'id')
+rm(warren_house_final, warren_senate, senate_shp)
+
 
 ########################
 
 # output csv
 setwd(data)
-fwrite(warren_submarket_final, "20241220_warren_speculative-investment-analysis-dataset-w-submarket.csv")
+fwrite(warren_join_final, "20250903_warren_speculative-investment-analysis-dataset-w-submarket.csv")
 
 ########### archive ########
 ##only need 4 year buy horizon
